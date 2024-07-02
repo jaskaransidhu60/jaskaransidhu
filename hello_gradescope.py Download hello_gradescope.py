1015c297e26fe47af35c148bc5366bd36c7249a7
jaskaran singh sidhu <sidhuja@oregonstate.edu>
2024-07-01 21:38:37 -0700

    Create hello_gradescope.py Download hello_gradescope.py

---
+++ hello_gradescope.py Download hello_gradescope.py
@@ -0,0 +1,8 @@
+# Name: jaskaran singh sidhu
+# OSU Email: sidhuja@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment: introducton
+# Due Date: 7/1/2-24
+# Description:  regarding first uploding for the gradoscope
+def gradescope_test():
+return 42
